30386d9259a6f4bac2f62a33297eb074892515d8
Eric Larson <ericlarsonbroom@gmail.com>
2020-11-23 21:53:26 -0700

    Added city variable

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,9 @@
+let country = "United States of America";
+let continent = "North America";
+let population = 30000000;
+let city = "Denver";
+console.log(country);
+console.log(continent);
+console.log(population);
+console.log(country, continent, population);
+console.log(city);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,22 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
-
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matias = "JM";
+let $function = 27;
+let PI = 3.1415;
+// keep variable names descriptive :)
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+// top is better than
+let job1 = "Programmer";
+let job2 = "Teacher";
+// Above is not descriptive, non-descriptive is bad
