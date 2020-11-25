4fd821cd5eeec4b905f26d15343912348f545a4c
Eric Larson <ericlarsonbroom@gmail.com>
2020-11-24 21:35:08 -0700

    Changed type of a variable

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,3 +1,4 @@
+/*
 let country = "United States of America";
 let continent = "North America";
 let population = 30000000;
@@ -7,3 +8,25 @@ console.log(continent);
 console.log(population);
 console.log(country, continent, population);
 console.log(city);
+*/
+
+//This is a comment (I am being ignored, I can write anything here)
+
+/*
+Everything here is commented out, I can comment out a bunch of stuff with this
+*/
+
+console.log(PI);
+
+console.log(true);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof "Jonas");
+
+// typeof operator show the type of the vaule
+// Only need the let on the initial declaration, I am making a value (value), and just changing what is in the box
+javascriptIsFun = "Hello";
+console.log(typeof javascriptIsFun);
