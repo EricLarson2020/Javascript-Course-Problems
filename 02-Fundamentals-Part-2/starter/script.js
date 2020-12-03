958bc6bfc8e1d6b8b5d5cf95f8931ee57516ed52
Eric Larson <ericlarsonbroom@gmail.com>
2020-12-03 13:11:46 -0700

    Added funtions declaratively and with expressions

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,97 @@
+// strict mode
+
+// stict mode: special mode to make code more secure
+// strict must be first thing in document
+"use strict";
+
+// stricts makes accidental errors harder to make
+// lets us see errors, tells us what we did wrong
+// makes visible errors in developer console
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+// It catches mispellings of variable names letting me know they are not defined
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive :D");
+*/
+
+// introduces short list of variable names for features that might be defined later
+// const interface = 'Audio';   will give error of reserved word
+// or const private = 534;
+// const if = 23; does not work either since it is already something in the language.
+
+// functions
+/*
+//  code in {} is called function body
+function logger() {
+  console.log("My name is Jonas");
+}
+
+logger();
+// calling / running / invoking the function.
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+  return juice;
+}
+
+fruitProcessor(5, 0);
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+
+console.log(appleOrangeJuice);
+*/
+
+// Function Declarations vs. Expressions
+
+/*
+// parameter is the placeholder in teh function, the argument is the actual value we use to fill in that palceholder.
+// function declaration
+const age1 = calcAge1(1991);
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
+}
+
+console.log(age1);
+
+// function expression : write function with no name, but store into variable, and varialbe is function
+
+
+// expression is you assign the function to a variable without giving it a name
+// Declaration is you declare the function and give it a name.
+// expression produce values => functions are just values
+
+// you can call function declarations before they are defined in the code
+// You cannot do this for expressions
+ */
+
+// Arrow Functions
+
+// added in ES6
+
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const age2 = calcAge2(1991);
+
+// arrow Function : Good for 1-liner functions : Dont need return keyword for this.
+const calcAge3 = (birthYear) => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  return `${firstName} retires in ${retirement} years`;
+};
+
+console.log(yearsUntilRetirement(1991, "Jonas"));
+
+// Difference- Arrow functions do not get a disk keyword.
